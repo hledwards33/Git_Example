f54d55e7f778aa66ae2e53be1d2bb1a65fcdb911
Harry Edwards <h.edwards.2@outlook.com>
2022-11-24 14:52:30 +0000

    Updated the run_model function within main.py

--- main.py
+++ main.py
@@ -6,15 +6,16 @@ from models.english_models.say_goodbye import Goodbye
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
 
+
 def run_model(name: str, language: str):
-
     if language == "English":
-        Hello.run()
-
+        Hello.run(name)
+        print("Do something...")
+        Goodbye.run(name)
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     logger.info("Model has started to run.")
-
+    run_model(name="Credit Risk", language="English")
     logger.info("Model has finished running.")
